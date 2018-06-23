9a66955aa04c0c9cc311cfafdb545ecabebf5d51
Danielyu520 <samyu520@126.com>
2018-06-23 23:18:25 +0800

    init the project

---
+++ PlayerDecode/PlayerDecode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PlayerDecode
+//
+//  Created by Danielyu on 2018/6/23.
+//  Copyright © 2018 Danielyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PlayerDecode/PlayerDecode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PlayerDecode
+//
+//  Created by Danielyu on 2018/6/23.
+//  Copyright © 2018 Danielyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
